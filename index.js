7b402e1cd031a7a7a7dc62c0dfefa8455da90358
sri <chegondisrianjaneyulu@gmail.com>
2024-10-06 21:31:14 +0530

    updta

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 
+app.use('/', (req, res) => {
+    res.send('hello world')
+})
 
 app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`)
