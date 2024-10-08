08aa37e4ae6e1ad9232f28471e49624d7ec879ff
sri <chegondisrianjaneyulu@gmail.com>
2024-10-08 11:19:17 +0530

    update

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 
-
 const app = express();
 const PORT = process.env.PORT || 3000
 
 const limiter = rateLimit({
     windowMs:4000,
     max: 3,
-    message: 'Too many requests, please try again after a second.'
+    message: 'Too many requests, please try again after a second.',
+    statusCode: 429,
+    headers: true,
+    handler: (req, res) => {
+        res.status(429).json({ message: 'Custom handler You have exceeded the limit!' + req.ip });
+    },
+    skip: (req) => {
+        const skipUrls = ['/status', '/languages']
+        return skipUrls.includes(req.path)
+    }
 })
 
-
 app.use(limiter)
+
+app.use('/status', (req, res) => {
+    res.send(['Success', 'Fail', 'Pending', 'Processing'])
+})
+
+app.use('/languages', (req, res) => {
+    res.send(['Java', 'JavaScript',  'Python', 'C++'])
+})
+
+app.use('/frameworks', (req, res) => {
+    res.send(['Express',  'ReactJS', 'NextJs', 'NestJS'])
+})
 
 app.use('/', (req, res) => {
     res.send("Welcome to api")
 })
+
+
 
 app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`)
